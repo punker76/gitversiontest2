e7ab7a6f6aaec78d75ee9a538cc1a4db67319215
Jan Karger <punkerat76@gmail.com>
2018-09-23 12:21:25 +0200

    initial commit

---
+++ src/Awesome/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Awesome
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
